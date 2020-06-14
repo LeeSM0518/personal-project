516609c90c2f7cd3f4f73a058589866a7942d79d
Sangmin Lee <nalsm98@naver.com>
2020-06-14 23:28:32 +0900

    인트로 페이지 구현 완료

---
+++ app/src/views/IntroPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="../assets/intro.png"
+    @click="() => $router.push('/login')"
+    width="100%"
+    height="100%"
+    class="ma-0"
+  />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
