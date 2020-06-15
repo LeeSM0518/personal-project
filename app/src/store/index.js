0b2bc8824416db5af3f89adbd5e6a799eab467dd
Sangmin Lee <nalsm98@naver.com>
2020-06-15 15:43:17 +0900

    Vuex 이용한 저장소 구현중
    * 회원 번호, 회원 이름, 회원 아이디 관리
    * 해당 필드들의 getters, setters 구현
    * 로그인 구현

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,11 +1,44 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { login } from '@/api/index';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    userId: -1,
+    name: '',
+    username: '',
+  },
+  getters: {
+    getUserId(state) {
+      return state.userId;
+    },
+    getName(state) {
+      return state.name;
+    },
+    getUsername(state) {
+      return state.username;
+    },
+  },
+  mutations: {
+    setName(state, name) {
+      state.name = name;
+    },
+    setUserId(state, userId) {
+      state.userId = userId;
+    },
+    setUsername(state, username) {
+      state.username = username;
+    },
+  },
+  actions: {
+    async LOGIN({ commit }, userData) {
+      const { data } = await login(userData);
+      commit('setName', data.name);
+      commit('setUserId', data.id);
+      commit('setUsername', userData.username);
+    },
+  },
   modules: {},
 });
