412f1cbe86ec2314ed66ebce0dc5d7ff15d9d9bc
Sangmin Lee <nalsm98@naver.com>
2020-09-03 19:11:40 +0900

    app 구현완료

---
+++ android/app/src/main/java/com/example/smartattendancesystem/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.smartattendancesystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("http://192.168.43.136:8081/");
+    }
+}
